99cd1c5296f1d1a6166b21866928aa996058e5fc
Joe Guinness <averagejoestats@gmail.com>
2025-11-25 12:13:27 -0500

    updates during lecture

--- hurdattools/R/hello.R
+++ hurdattools/R/hello.R
@@ -3,10 +3,11 @@
 #' This function prints a friendly greeting.
 #'
 #' @param name A character string with the name to greet.
+#' @param greeting A character string with a greeting.
 #' @return A character string containing the greeting.
 #' @examples
 #' hello("Tengis")
 #' @export
-hello <- function(name = "world") {
-    paste0("Hello, ", name, "!")
+hello <- function(name = "world", greeting = "Have a great day!") {
+    paste0("Hello, ", name, "!", " ", greeting)
 }
